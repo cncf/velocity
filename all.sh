ff33b5d173228a762a22695fe40ff38e9f991d52
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-04-27 16:13:39 +0200

    lukaszgryglicki-mapping-ready-before-final-processing-backup

---
+++ all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ruby analysis.rb data_201601.csv projects_201601.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201602.csv projects_201602.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201603.csv projects_201603.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201604.csv projects_201604.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201605.csv projects_201605.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201606.csv projects_201606.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201607.csv projects_201607.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201608.csv projects_201608.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201609.csv projects_201609.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201610.csv projects_201610.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201611.csv projects_201611.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201612.csv projects_201612.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201701.csv projects_201701.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201702.csv projects_201702.csv hints.csv urls.csv defmaps.csv
+ruby analysis.rb data_201703.csv projects_201703.csv hints.csv urls.csv defmaps.csv
