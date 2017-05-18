c27f8f2d5664ba44a92fe8605c311691c8db96fd
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-05-18 14:29:26 +0200

    lukaszgryglicki-added-cmi-to-cncf-projects-chart2

--- shells/top30_201605_201704.sh
+++ shells/top30_201605_201704.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ruby add_linux.rb data/data_top30_201605_201704.csv data/data_linux.csv 2016-05-01 2017-05-01
 ruby merger.rb data/data_top30_201605_201704.csv data/data_cncf_projects.csv
 ruby merger.rb data/data_top30_201605_201704.csv data/webkit_201605_201704.csv
 ruby analysis.rb data/data_top30_201605_201704.csv projects/projects_top30_201605_201704.csv map/hints.csv map/urls.csv map/defmaps.csv map/skip.csv map/ranges.csv
