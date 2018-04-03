6f2763f8d73ea0f147d65752855f844e1051ad29
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-03 09:03:29 +0000

    Top 30 chart data generated
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- shells/unlimited_20170401_20180401.sh
+++ shells/unlimited_20170401_20180401.sh
@@ -2,41 +2,41 @@
 echo "Restoring BigQuery output"
 cp data/unlimited_output_20170401_20180401.csv data/unlimited.csv
 echo "Adding Linux kernel data"
-ruby add_linux.rb data/unlimited.csv data/data_linux.csv 2017-04-01 2018-04-01
+ruby add_linux.rb data/unlimited.csv data/data_linux.csv 2017-04-01 2018-04-01 || exit 1
 echo "Adding GitLab data"
-ruby add_external.rb data/unlimited.csv data/data_gitlab.csv 2017-04-01 2018-04-01 gitlab gitlab/GitLab
+ruby add_external.rb data/unlimited.csv data/data_gitlab.csv 2017-04-01 2018-04-01 gitlab gitlab/GitLab || exit 1
 echo "Adding/Updating Cloud Foundry Projects"
 # This uses "force" mode to update Cloud Foundry values to lower ones (this is because we have special query output for CF projects which skips more bots, so lower values are expected)
-ruby merger.rb data/unlimited.csv data/cf_20170401_20180401.csv force
+ruby merger.rb data/unlimited.csv data/cf_20170401_20180401.csv force || exit 1
 # Don't forget to add exception to map/ranges.csv when adding projects pulled with different BigQuery (specially with 0s for issues, PRs etc)
 echo "Adding/Updating CNCF Projects"
-ruby merger.rb data/unlimited.csv data/cncf_20170401_20180401.csv
+ruby merger.rb data/unlimited.csv data/cncf_20170401_20180401.csv || exit 1
 echo "Adding/Updating OpenStack case"
-ruby merger.rb data/unlimited.csv data/openstack_20170401_20180401.csv
+ruby merger.rb data/unlimited.csv data/openstack_20170401_20180401.csv || exit 1
 echo "Adding/Updating Apache case"
-ruby merger.rb data/unlimited.csv data/apache_20170401_20180401.csv
+ruby merger.rb data/unlimited.csv data/apache_20170401_20180401.csv || exit 1
 echo "Adding/Updating Chromium case"
-ruby merger.rb data/unlimited.csv data/data_chromium_20170401_20180401.csv
+ruby merger.rb data/unlimited.csv data/data_chromium_20170401_20180401.csv || exit 1
 echo "Adding/Updating openSUSE case"
-ruby merger.rb data/unlimited.csv data/opensuse_20170401_20180401.csv
+ruby merger.rb data/unlimited.csv data/opensuse_20170401_20180401.csv || exit 1
 echo "Adding/Updating AutomotiveGradeLinux (AGL) case"
-ruby merger.rb data/unlimited.csv data/data_agl_201700401_20180401.csv
+ruby merger.rb data/unlimited.csv data/data_agl_20170401_20180401.csv || exit 1
 echo "Adding/Updating LibreOffice case"
-ruby merger.rb data/unlimited.csv data/lo_20170401_20180401.csv
+ruby merger.rb data/unlimited.csv data/lo_20170401_20180401.csv || exit 1
 echo "Adding/Updating FreeBSD Projects"
-ruby merger.rb data/unlimited.csv data/freebsd_20170401_20180401.csv
+ruby merger.rb data/unlimited.csv data/freebsd_20170401_20180401.csv || exit 1
 echo "Analysis"
-ruby analysis.rb data/unlimited.csv projects/unlimited_both.csv map/hints.csv map/urls.csv map/defmaps.csv map/skip.csv map/ranges_sane.csv
+ruby analysis.rb data/unlimited.csv projects/unlimited_both.csv map/hints.csv map/urls.csv map/defmaps.csv map/skip.csv map/ranges_sane.csv || exit 1
 echo "Updating OpenStack projects using their bug tracking data"
-ruby update_projects.rb projects/unlimited_both.csv data/data_openstack_bugs_20180401_20180401.csv -1
+ruby update_projects.rb projects/unlimited_both.csv data/data_openstack_bugs_20180401_20180401.csv -1 || exit 1
 echo "Updating Apache Projects using Jira data"
-ruby update_projects.rb projects/unlimited_both.csv data/data_apache_jira_20170401_20180401.csv -1
+ruby update_projects.rb projects/unlimited_both.csv data/data_apache_jira_20170401_20180401.csv -1 || exit 1
 echo "Updating Chromium project using their bug tracking data"
-ruby update_projects.rb projects/unlimited_both.csv data/data_chromium_bugtracker_20170401_20180401.csv -1
+ruby update_projects.rb projects/unlimited_both.csv data/data_chromium_bugtracker_20170401_20180401.csv -1 || exit 1
 echo "Updating LibreOffice project using their git repo"
-ruby update_projects.rb projects/unlimited_both.csv data/data_lo_git_20170401_20180401.csv -1
+ruby update_projects.rb projects/unlimited_both.csv data/data_lo_git_20170401_20180401.csv -1 || exit 1
 echo "Updating FreeBSD project using their repos SVN data"
-ruby update_projects.rb projects/unlimited_both.csv data/data_freebsd_svn_20170401_20180401.csv -1
+ruby update_projects.rb projects/unlimited_both.csv data/data_freebsd_svn_20170401_20180401.csv -1 || exit 1
 echo "Generating Projects Ranks statistics"
 ./shells/report_cncf_project_ranks.sh
 ./shells/report_other_project_ranks.sh
