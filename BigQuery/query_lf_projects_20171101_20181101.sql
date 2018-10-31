712264a2209750c199df0096504685a9a284b239
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-31 17:35:00 +0000

    Working on LF projects
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ BigQuery/query_lf_projects_20171101_20181101.sql
@@ -0,0 +1,81 @@
+SELECT
+  org,
+  repo,
+  sum(activity) AS activity,
+  sum(comments) AS comments,
+  sum(prs) AS prs,
+  sum(commits) AS commits,
+  sum(issues) AS issues,
+  EXACT_COUNT_DISTINCT(author_email) AS authors_alt2,
+  GROUP_CONCAT(STRING(author_name)) AS authors_alt1,
+  GROUP_CONCAT(STRING(author_email)) AS authors
+FROM (
+SELECT
+  org.login AS org,
+  repo.name AS repo,
+  count(*) AS activity,
+  SUM(IF(type in ('IssueCommentEvent', 'PullRequestReviewCommentEvent', 'CommitCommentEvent'), 1, 0)) as comments,
+  SUM(IF(type = 'PullRequestEvent', 1, 0)) AS prs,
+  SUM(IF(type = 'PushEvent', 1, 0)) AS commits,
+  SUM(IF(type = 'IssuesEvent', 1, 0)) AS issues,
+  IFNULL(REPLACE(JSON_EXTRACT(payload, '$.commits[0].author.email'), '"', ''), '(null)') AS author_email,
+  IFNULL(REPLACE(JSON_EXTRACT(payload, '$.commits[0].author.name'), '"', ''), '(null)') AS author_name
+FROM 
+  (SELECT * from
+    TABLE_DATE_RANGE([githubarchive:day.],TIMESTAMP('2017-11-01'),TIMESTAMP('2018-10-31'))
+  )
+WHERE
+  (
+    org.login IN (
+      'alljoyn','cip-project','cloudfoundry','cncf','codeaurora-unofficial','coreinfrastructure','Dronecode',
+      'edgexafoundry','fdio-stack','fluent','fossology','frrouting','grpc','hyperledger','iovisor','iotivity',
+      'JSFoundation','Kinetic','kubernetes','letsencrypt','linkerd','LinuxStandardBase','nodejs','odpi','OAI',
+      'opencontainers','openmainframeproject','opensecuritycontroller','openvswitch','openchain','opendaylight',
+      'openhpc','OpenMAMA','opensds','open-switch','opentracing','opnfv','pndaproject','prometheus','RConsortium',
+      'rethinkdb','SNAS','spdx','todogroup','xen-project','zephyrproject-rtos', 'containerd', 'rkt', 
+      'kubernetes-helm', 'kubernetes-client', 'kubernetes-incubator', 'coredns', 'grpc-ecosystem',
+      'containernetworking', 'crosscloudci', 'cloudevents', 'openeventing', 'envoyproxy', 'jaegertracing',
+      'theupdateframework', 'rook', 'vitessio', 'telepresenceio', 'helm', 'goharbor', 'kubernetes-csi',
+      'nats-io', 'open-policy-agent', 'spiffe',  'etcd-io', 'tikv', 'cortexproject', 'buildpack', 'falcosecurity',
+      'OpenObservability'
+    )
+    OR repo.name IN (
+      'automotive-grade-linux/docs-agl','joeythesaint/cgl-specification',
+      'opencord/cord','cncf/cross-cloud',
+      'cregit/cregit','diamon/diamon-www-data','JanusGraph/janusgraph','opennetworkinglab/onos',
+      'brunopulis/awesome-a11y','obrienlabs/onap-root','ni/linux','Samsung/TizenRT',
+      'docker/containerd', 'coreos/rkt', 'GoogleCloudPlatform/kubernetes', 
+      'lyft/envoy', 'uber/jaeger', 'BuoyantIO/linkerd', 'apcera/nats', 'apcera/gnatsd',
+      'docker/notary', 'youtube/vitess', 'appc/cni', 'miekg/coredns', 'coreos/rocket',
+      'rktproject/rkt', 'datawire/telepresence', 'RichiH/OpenMetrics', 'vmware/harbor',
+      'coreos/etcd', 'pingcap/tikv', 'weaveworks/cortex', 'weaveworks/prism',
+      'weaveworks/frankenstein', 'draios/falco'
+    )
+  )
+  and type in ('IssueCommentEvent', 'PullRequestEvent', 'PushEvent', 'IssuesEvent', 'PullRequestReviewCommentEvent', 'CommitCommentEvent')
+  and (
+    type = 'PushEvent' or (
+      actor.login not like '%bot%'
+      and actor.login not in (
+        'CF MEGA BOT','CAPI CI','CF Buildpacks Team CI Server','CI Pool Resource','I am Groot CI','CI (automated)',
+        'Loggregator CI','CI (Automated)','CI Bot','cf-infra-bot','CI','cf-loggregator','bot','CF INFRASTRUCTURE BOT',
+        'CF Garden','Container Networking Bot','Routing CI (Automated)','CF-Identity','BOSH CI','CF Loggregator CI Pipeline',
+        'CF Infrastructure','CI Submodule AutoUpdate','routing-ci','Concourse Bot','CF Toronto CI Bot','Concourse CI',
+        'Pivotal Concourse Bot','RUNTIME OG CI','CF CredHub CI Pipeline','CF CI Pipeline','CF Identity',
+        'PCF Security Enablement CI','CI BOT','Cloudops CI','hcf-bot','Cloud Foundry Buildpacks Team Robot',
+        'CF CORE SERVICES BOT','PCF Security Enablement','fizzy bot','Appdog CI Bot','CF Tribe','Greenhouse CI',
+        'fabric-composer-app','iotivity-replication','SecurityTest456','odl-github','opnfv-github' 
+      )
+    )
+  )
+GROUP BY org, repo, author_email, author_name
+)
+GROUP BY org, repo
+HAVING 
+  authors_alt2 > 5
+  AND comments > 50
+  AND prs > 20
+  AND commits > 20
+AND issues > 20
+ORDER BY activity DESC
+;
--- docs/linuxfoundation_chart_creation.md
+++ docs/linuxfoundation_chart_creation.md
@@ -9,27 +9,27 @@
 `analysis.rb` can be used to create data for a Cloud Native Computing Foundation projects bubble chart such as this one
 ![sample chart](./linuxfoundation_chart_example.png?raw=true "CNCF projects")
 
-The chart itself can be generated in a [google sheet](https://docs.google.com/spreadsheets/d/1_DIvQpaPRecRONWeTh5pp3WOgbGcsY4JOPMBisizJqg/)
+The chart itself can be generated in a [google sheet](https://docs.google.com/spreadsheets/d/1oMiQySj7Nd9Q6Qm5RGT2WED8Rlhc8sTRR7p13IO6Lac/edit?usp=sharing).
 or as a stand-alone [html page](../charts/LF_bubble_chart.html). Details on usage of google chart api are [here](https://developers.google.com/chart/interactive/docs/gallery/bubblechart). The first option is a copy/paste of resulting data whereas the second presents more control to the look of the chart. Refer to the [Bubble Chart Generator](other_notes.md#bubble-chart-generator) for automatic html creation.
 
 ### Chart data
 Go to this [CNCF page](https://www.linuxfoundation.org/projects/) to find a list of current projects.
 
-For every project, find a github repo and add it to a query such as [this one](BigQuery/query_lf_projects_20170801_20180801.sql) appropriately - either as an org or a single repo. If a project does not have a GitHub repo or only lists a mirror, skip it for now but later add manually. Update the time range.
+For every project, find a github repo and add it to a query such as [this one](BigQuery/query_lf_projects_20171101_20181101.sql) appropriately - either as an org or a single repo. If a project does not have a GitHub repo or only lists a mirror, skip it for now but later add manually. Update the time range.
 
-Copy the results to a file with proper name `data/data_lf_projects_20170801_20180801.csv`
+Copy the results to a file with proper name `data/data_lf_projects_20171101_20181101.csv`
 
 <b>Add CNCF projects</b>
 - CNCF Projects case
-- We have a line in `ruby merger.rb data/unlimited.csv data/data_cncf_projects.csv` which needs to be changed to `ruby merger.rb data/unlimited.csv data/data_cncf_projects_20170801_20180801.csv`
-- Copy: `cp BigQuery/query_cncf_projects.sql BigQuery/query_cncf_projects_20170801_20180801.sql`, update conditions: `BigQuery/query_cncf_projects_20170801_20180801.sql`
+- We have a line in `ruby merger.rb data/unlimited.csv data/data_cncf_projects.csv` which needs to be changed to `ruby merger.rb data/unlimited.csv data/data_cncf_projects_20171101_20181101.csv`
+- Copy: `cp BigQuery/query_cncf_projects.sql BigQuery/query_cncf_projects_20171101_20181101.sql`, update conditions: `BigQuery/query_cncf_projects_20171101_20181101.sql`
 - Run on BigQuery and do the same as in the CF case. The final output file will be: `data/data_cncf_projects_201606_201705.csv`
-- Final line should be: `ruby merger.rb data/data_lf_projects_20170801_20180801.csv data/data_cncf_projects_20170801_20180801.csv`.
+- Final line should be: `ruby merger.rb data/data_lf_projects_20171101_20181101.csv data/data_cncf_projects_20171101_20181101.csv`.
 
 
 <b>Add Linux data</b>
 Try running this from the velocity project's root folder:
-`ruby add_linux.rb data/data_lf_projects_20170801_20180801.csv data/data_linux.csv 2017-08-01 2018-08-01`
+`ruby add_linux.rb data/data_lf_projects_20171101_20181101.csv data/data_linux.csv 2017-08-01 2018-08-01`
 - A message will be shown: `Data range not found in data/data_linux.csv: 2017-11-01 - 2017-11-01`. That means you need to add a new data range for Linux in file: `data/data_linux.csv`
 - Go to: `https://lkml.org/lkml/2017` and sum-up monthly email counts for the time period of interest, in this case, 263996.
 - Add a row for the time period in `data/data_linux.csv`: `torvalds,torvalds/linux,2016-11-01,2017-11-01,0,0,0,0,263996` - You will see that now we only have the "emails" column. Other columns must be feteched from the linux kernel repo using the `cncf/gitdm` analysis:
