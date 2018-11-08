824de39f8c7a6468a033a05809d31280f26fc820
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-08 10:35:48 +0000

    Added LibreOffice Bugzilla bugs counter script using Bitergia perceval as a data-source/library
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ libreoffice_bugzilla.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: please provide date from YYYY-MM-DD HH:MI:SS"
+  exit 1
+fi
+if [ -z "$2" ]
+then
+  echo "$0: please provide date to YYYY-MM-DD HH:MI:SS"
+  exit 2
+fi
+# possibly add -C 1 -D 30
+./count_bugzilla.py -f "$1" -t "$2" -u 'https://bugs.freedesktop.org' -p "LibreOffice"  ${@:3:99}
