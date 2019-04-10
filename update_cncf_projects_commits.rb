5ca8e622b74fdbcc35f5b87a9017720c441a2793
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-10 11:35:49 +0000

    Added script to update commits in all CNCF projects for a given date range
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ update_cncf_projects_commits.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+# require 'pry'
+require 'yaml'
+
+if ENV['PG_PASS'].nil?
+  puts "You need to set PG_PASS=..."
+  exit 1
+end
+
+from = ARGV[0]
+to = ARGV[1]
+
+if from.nil? || to.nil?
+  puts "You need to set provide dt-from and dt-to arguments"
+  exit 2
+end
+  
+data = YAML.load_file '/root/dev/go/src/github.com/cncf/devstats/projects.yaml'
+`echo 'project,key,value' > data/data_cncf_update.csv`
+data['projects'].each do |project|
+  next if project[0] == 'all'
+  db = project[1]['psql_db']
+  name = project[1]['name']
+  disabled = project[1]['disabled']
+  next if disabled
+  puts "#{db} -> #{name}"
+  `./shells/get_git_commits_count.sh "#{db}" "#{from}" "#{to}"`
+  commits=`cat git.log | wc -l`
+  puts "#{name} commits: #{commits}"
+  `echo -n "#{name},commits,#{commits}" >> data/data_cncf_update.csv`
+end
+
