3b82bf30d7d9ad3dc6b8b807f8d5cf91caf91e14
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-11 16:30:12 +0000

    Update OpenStack commits authors counting algorithm 2
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- openstack/openstack.sh
+++ openstack/openstack.sh
@@ -4,4 +4,4 @@ then
   echo "$0: you need to provide date from and date to"
   exit 1
 fi
-GHA2DB_CSVOUT="data_openstack_$1_$2.csv" GHA2DB_LOCAL=1 PG_DB=contrib runq ./openstack_data.sql {{from}} $1 {{to}} $2
+GHA2DB_CSVOUT="data_openstack_$1_$2.csv" GHA2DB_LOCAL=1 PG_DB=contrib runq ./openstack_data.sql {{exclude_bots}} "`cat ~/dev/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql`" {{from}} $1 {{to}} $2
