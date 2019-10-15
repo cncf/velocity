13cbc970abe5561d16075078125a8ba93f364ab8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-15 05:49:52 +0000

    Adding Longhorn
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- BigQuery/velocity_lf.sql
+++ BigQuery/velocity_lf.sql
@@ -45,7 +45,7 @@ WHERE
       'kubernetes-sidecars', 'kubernetes-tools', 'cdfoundation', 'spinnaker', 'tektoncd', 'jenkinsci',
       'iovisor', 'mininet', 'opennetworkinglab', 'p4lang', 'OpenBMP', 'tungstenfabric', 'opencord',
       'networkservicemesh', 'cri-o', 'open-telemetry', 'openebs', 'graphql', 'thanos-io', 'fluxcd',
-      'in-toto', 'strimzi', 'kubevirt'
+      'in-toto', 'strimzi', 'kubevirt', 'longhorn'
     )
     OR repo.name IN (
       'automotive-grade-linux/docs-agl','joeythesaint/cgl-specification','cncf/cross-cloud',
@@ -58,7 +58,8 @@ WHERE
       'coreos/etcd', 'pingcap/tikv', 'weaveworks/cortex', 'weaveworks/prism', 'knative/build',
       'weaveworks/frankenstein', 'draios/falco', 'alibaba/Dragonfly', 'Azure/brigade',
       'ligato/networkservicemesh', 'improbable-eng/promlts', 'improbable-eng/thanos',
-      'weaveworks/flux', 'EnMasseProject/barnabas', 'ppatierno/barnabas', 'ppatierno/kaas'
+      'weaveworks/flux', 'EnMasseProject/barnabas', 'ppatierno/barnabas', 'ppatierno/kaas',
+      'rancher/longhorn'
     )
   )
   and type in (
