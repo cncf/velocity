0fdb64b9dd4eab2586fae68640f4813496883375
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-12 12:52:34 +0000

    AGL data collected
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- agl/run_multirepo_range.sh
+++ agl/run_multirepo_range.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # ./multirepo_range.sh meta-agl meta-agl-demo meta-agl-devel meta-agl-extra meta-altera meta-boundary meta-freescale meta-intel meta-intel-iot-security meta-oic meta-openembedded meta-qcom meta-qt5 meta-raspberrypi meta-renesas meta-renesas-rcar-gen3 meta-rust meta-sdl meta-security-isafw meta-ti meta-updater meta-virtualization poky
-./multirepo_range.sh poky meta-*
+# ./multirepo_range.sh meta-* bsp/* external/*
+./multirepo_range.sh meta-* bsp/*
