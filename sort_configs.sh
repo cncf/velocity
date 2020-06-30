9c5cf48636467eb64ee02596cb974a037d474c48
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-06-30 07:31:36 +0000

    Added 11 new Sandbox projects 5 [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sort_configs.sh
+++ sort_configs.sh
@@ -3,3 +3,8 @@ sed -i '1d' map/hints.csv
 cat map/hints.csv | sort | uniq > out
 echo 'repo,project' > map/hints.csv
 cat out >> map/hints.csv
+
+sed -i '1d' map/defmaps.csv
+cat map/defmaps.csv | sort | uniq > out
+echo 'name,project' > map/defmaps.csv
+cat out >> map/defmaps.csv
