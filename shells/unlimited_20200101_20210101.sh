069b9d80c6140ae6b74332cddb0e24cb16b49cb3
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-01-11 10:06:09 +0000

    Top 30 chart generation wip
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- shells/unlimited_20200101_20210101.sh
+++ shells/unlimited_20200101_20210101.sh
@@ -31,18 +31,18 @@ cp map/defmaps.csv map/defmaps_oo.csv
 cat map/defmaps_merged_openstack.csv >> map/defmaps_oo.csv
 # ruby analysis.rb data/unlimited.csv projects/unlimited_both.csv map/hints.csv map/urls.csv map/defmaps.csv map/skip.csv map/ranges_sane.csv
 ruby analysis.rb data/unlimited.csv projects/unlimited_both.csv map/hints.csv map/urls.csv map/defmaps_oo.csv map/skip_special.csv map/ranges_sane.csv
-echo "Updating Apache Projects using Jira data"
-ruby update_projects.rb projects/unlimited_both.csv data/data_apache_jira_20200101_20210101.csv -1
-echo "Updating OpenStack projects using their bug tracking data"
-ruby update_projects.rb projects/unlimited_both.csv data/data_openstack_bugs_20200101_20210101.csv -1
+#echo "Updating Apache Projects using Jira data"
+#ruby update_projects.rb projects/unlimited_both.csv data/data_apache_jira_20200101_20210101.csv -1
+#echo "Updating OpenStack projects using their bug tracking data"
+#ruby update_projects.rb projects/unlimited_both.csv data/data_openstack_bugs_20200101_20210101.csv -1
 # This is for merged OpenStack into a single project
-ruby update_projects.rb projects/unlimited_both.csv data/data_openstack_all_2020-01-01_2021-01-01.csv -1
-echo "Updating Chromium project using their bug tracking data"
-ruby update_projects.rb projects/unlimited_both.csv data/data_chromium_bugtracker_20200101_20210101.csv -1
-echo "Updating LibreOffice project using their git repo"
-ruby update_projects.rb projects/unlimited_both.csv data/data_libreoffice_git_20200101_20210101.csv -1
-echo "Updating FreeBSD data from SVN logs"
-ruby update_projects.rb projects/unlimited_both.csv ./data/data_freebsd_svn_20200101_20210101.csv -1
+#ruby update_projects.rb projects/unlimited_both.csv data/data_openstack_bugs_20200101_20210101.csv -1
+#echo "Updating Chromium project using their bug tracking data"
+#ruby update_projects.rb projects/unlimited_both.csv data/data_chromium_bugtracker_20200101_20210101.csv -1
+#echo "Updating LibreOffice project using their git repo"
+#ruby update_projects.rb projects/unlimited_both.csv data/data_libreoffice_git_20200101_20210101.csv -1
+#echo "Updating FreeBSD data from SVN logs"
+#ruby update_projects.rb projects/unlimited_both.csv ./data/data_freebsd_svn_20200101_20210101.csv -1
 echo "Prioritizing LF projects data"
 PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_lf_20200101_20210101.csv -1
 echo "Prioritizing CNCF projects data"
