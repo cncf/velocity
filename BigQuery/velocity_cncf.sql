eea9781d8ede976e0384c67be1a2ed77aad4291f
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-01-29 21:27:37 +0000

    Fix org name
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- BigQuery/velocity_cncf.sql
+++ BigQuery/velocity_cncf.sql
@@ -46,7 +46,7 @@ where
       'chaos-mesh', 'serverlessworkflow', 'wayfair-tremor', 'metal3-io', 'openservicemesh', 'tremor-rs',
       'getporter', 'keylime', 'backstage', 'schemahero', 'cert-manager', 'openkruise', 'kruiseio',
       'tinkerbell', 'pravega', 'kyverno', 'buildpacks', 'gitops-working-group', 'piraeusdatastore',
-      'curiefense', 'distribution', 'kubeovn', 'Athenz'
+      'curiefense', 'distribution', 'kubeovn', 'AthenZ'
     )
     or repo.name in (
       'docker/containerd', 'coreos/rkt', 'GoogleCloudPlatform/kubernetes', 
