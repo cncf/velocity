b6702e6fd7961f588a34c9dd55ab0c351eff9d42
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-04-15 07:29:35 +0000

    Rename ingraind to foniod
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- BigQuery/velocity_cncf.sql
+++ BigQuery/velocity_cncf.sql
@@ -46,7 +46,8 @@ where
       'chaos-mesh', 'serverlessworkflow', 'wayfair-tremor', 'metal3-io', 'openservicemesh', 'tremor-rs',
       'getporter', 'keylime', 'backstage', 'schemahero', 'cert-manager', 'openkruise', 'kruiseio',
       'tinkerbell', 'pravega', 'kyverno', 'buildpacks', 'gitops-working-group', 'piraeusdatastore',
-      'curiefense', 'distribution', 'kubeovn', 'AthenZ', 'openyurtio', 'ingraind', 'tricksterproxy'
+      'curiefense', 'distribution', 'kubeovn', 'AthenZ', 'openyurtio', 'ingraind', 'tricksterproxy',
+      'foniod'
     )
     or repo.name in (
       'docker/containerd', 'coreos/rkt', 'GoogleCloudPlatform/kubernetes', 
