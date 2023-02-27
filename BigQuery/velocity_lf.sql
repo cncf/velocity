9e24a00598521b5097f6995abb5b63cb38d5140b
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-02-27 10:31:27 +0000

    Update Carvel & OpenCost config 2
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- BigQuery/velocity_lf.sql
+++ BigQuery/velocity_lf.sql
@@ -67,7 +67,8 @@ WHERE
       'devstream-io', 'kubedl-io', 'kubevela', 'hexa-org', 'konveyor', 'external-secrets', 'krustlet', 'openembedded',
       'Serverless-Devs', 'ServerlessTool', 'ContainerSSH', 'openfga', 'lima-vm', 'k14s', 'kubereboot', 'istio',
       'inclavare-containers', 'notaryproject', 'merbridge', 'devspace-cloud', 'covexo', 'capsule-rs', 'project-zot',
-      'paralus', 'carina-io', 'ko-build', 'opcr-io', 'werf', 'kubescape', 'openelb', 'tektoncd-catalog'
+      'paralus', 'carina-io', 'ko-build', 'opcr-io', 'werf', 'kubescape', 'openelb', 'tektoncd-catalog',
+      'opencost', 'carvel-dev'
     )
     OR repo.name IN (
       'joeythesaint/cgl-specification','cncf/cross-cloud', 'deislabs/oras', 'shizhMSFT/oras',
