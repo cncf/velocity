bd31af9e41a8dd4b596f265fa4ea310b89ff90dd
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-07-14 05:44:09 +0000

    Add 'buildpacks-community' org to buildpacks
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- BigQuery/velocity_cncf.sql
+++ BigQuery/velocity_cncf.sql
@@ -62,7 +62,7 @@ where
       'project-zot', 'paralus', 'carina-io', 'ko-build', 'opcr-io', 'werf', 'kubescape', 'openelb',
       'opencost', 'carvel-dev', 'inspektor-gadget', 'clusternet', 'sealerio', 'keycloak', 'armadaproject',
       'headlamp-k8s', 'slimtoolkit', 'sockerslim', 'sustainable-computing-io', 'pipe-cd', 'xline-kv',
-      'hwameistor', 'microcks', 'kubeclipper', 'kubeclipper-labs', 'kubeflow'
+      'hwameistor', 'microcks', 'kubeclipper', 'kubeclipper-labs', 'kubeflow', 'buildpacks-community'
     )
     or repo.name in (
       'docker/containerd', 'coreos/rkt', 'GoogleCloudPlatform/kubernetes', 'cncf/sig-app-delivery',
--- BigQuery/velocity_lf.sql
+++ BigQuery/velocity_lf.sql
@@ -71,7 +71,7 @@ WHERE
       'carvel-dev', 'inspektor-gadget', 'clusternet', 'cdevents', 'ortelius', 'pyrsia', 'screwdriver-cd',
       'shipwright-io', 'sealerio', 'keycloak', 'armadaproject', 'devspace-sh', 'tellerops', 'headlamp-k8s',
       'slimtoolkit', 'sockerslim', 'sustainable-computing-io', 'pipe-cd', 'xline-kv', 'hwameistor', 'microcks',
-      'kubeclipper', 'kubeclipper-labs', 'kubeflow'
+      'kubeclipper', 'kubeclipper-labs', 'kubeflow', 'buildpacks-community'
     )
     OR repo.name IN (
       'joeythesaint/cgl-specification','cncf/cross-cloud', 'deislabs/oras', 'shizhMSFT/oras',
