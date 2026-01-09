5458c77a03ae876cee7a7d3f78e398abb8724946
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-01-09 11:58:10 +0100

    Update scripts
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- shells/get_git_commits_count.sh
+++ shells/get_git_commits_count.sh
@@ -22,7 +22,7 @@ then
 fi
 if [ -z "$REPOS" ]
 then
-  repos=$(db.sh psql "${1}" -tAc "select distinct name from gha_repos")
+  repos=$(db.sh psql "${1}" -P pager=off -tAc "select distinct name from gha_repos")
 else
   repos="${REPOS//[,\']/}"
 fi
@@ -53,6 +53,6 @@ cat "${log}" | sort | uniq >> out && mv out "${log}"
 ls -l "${log}"
 cp "${log}" /tmp/
 bots=`cat ~/dev/go/src/github.com/cncf/devstats/util_sql/only_bots.sql`
-commits=`db.sh psql gha -q -c 'create temp table tcom(c text, a text, sha varchar(40))' -c "copy tcom from '/tmp/git.log' with (format csv)" -c "create temp table bots as select distinct email from gha_actors_emails where actor_id in (select id from gha_actors where lower(login) $bots)" -c "select count(distinct sha) from tcom where a not in (select email from bots) and c not in (select email from bots)" -tAc 'drop table bots' -c 'drop table tcom'`
+commits=`db.sh psql gha -P pager=off -q -c 'create temp table tcom(c text, a text, sha varchar(40))' -c "copy tcom from '/tmp/git.log' with (format csv)" -c "create temp table bots as select distinct email from gha_actors_emails where actor_id in (select id from gha_actors where lower(login) $bots)" -c "select count(distinct sha) from tcom where a not in (select email from bots) and c not in (select email from bots)" -tAc 'drop table bots' -c 'drop table tcom'`
 echo "${1}: ${2} - ${3}: ${commits} commits"
 echo "$commits" > commits.txt
