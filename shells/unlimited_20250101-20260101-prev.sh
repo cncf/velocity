c35852eef72fc07d7e2bc3a2085eccc5bf619d29
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-02-20 12:51:13 +0100

    Top 30 projects data work - WIP 2
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- shells/unlimited_20250101-20260101-prev.sh
+++ shells/unlimited_20250101-20260101-prev.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 if [ ! -z "GENERATE" ]
 then
-  echo "Running BigQuery for top30_without_committers"
-  ./run_bq_standard.sh top30_without_committers 20250101 20260101
-  echo "Enriching commits data s it is missing on GHA since Oct 2025"
-  ./tools/enrich_authors/enrich_authors -in data/data_top30_without_committers_projects_20250101_20260101.csv -out data/data_top30_without_committers_projects_20250101_20260101.enriched.csv -from 2025-01-01 -to 2026-01-01 -forks all_forks.json
-  echo "Copying enriched and raw files"
-  cp data/data_top30_without_committers_projects_20250101_20260101.csv data/data_top30_without_committers_projects_20250101_20260101.raw.csv
-  cp data/data_top30_without_committers_projects_20250101_20260101.enriched.csv data/data_top30_without_committers_projects_20250101_20260101.csv
+  ./run_bq_standard.sh top30_new 20250101 20260101
 fi
 echo "Restoring BigQuery output"
-cp data/data_top30_without_committers_projects_20250101_20260101.csv data/unlimited.csv
+cp data/data_top30_new_projects_20250101_20260101.csv data/unlimited.csv
 echo "Adding Linux kernel data"
 ruby add_linux.rb data/unlimited.csv data/data_linux.csv 2025-01-01 2026-01-01
 # Don't forget to add exception to map/ranges.csv when adding projects pulled with different BigQuery (specially with 0s for issues, PRs etc)
