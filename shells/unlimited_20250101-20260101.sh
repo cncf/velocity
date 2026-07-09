f3551f43563fea9aa281d8719f3276d28c7a13e5
Łukasz Gryglicki <lgryglicki@cncf.io>
2026-07-09 16:04:53 +0200

    Top 30 OSS Projects - wip 1
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- shells/unlimited_20250101-20260101.sh
+++ shells/unlimited_20250101-20260101.sh
@@ -1,36 +1,31 @@
 #!/bin/sh
 if [ ! -z "GENERATE" ]
 then
-  echo "Running BigQuery for top30_without_committers"
-  ./run_bq_standard.sh top30_without_committers 20250101 20260101
-  echo "Enriching commits data s it is missing on GHA since Oct 2025"
-  ./tools/enrich_authors/enrich_authors -in data/data_top30_without_committers_projects_20250101_20260101.csv -out data/data_top30_without_committers_projects_20250101_20260101.enriched.csv -from 2025-01-01 -to 2026-01-01 -forks all_forks.json
-  echo "Copying enriched and raw files"
-  cp data/data_top30_without_committers_projects_20250101_20260101.csv data/data_top30_without_committers_projects_20250101_20260101.raw.csv
-  cp data/data_top30_without_committers_projects_20250101_20260101.enriched.csv data/data_top30_without_committers_projects_20250101_20260101.csv
+  # ./run_bq_standard.sh top30 20240701 20250101
+  # ./run_bq_standard.sh top30 20250101 20250701
+  # OUT=data/data_top30_projects_20240701_20250701.csv ./merge_bq.rb data/data_top30_projects_20240701_20250101.csv data/data_top30_projects_20250101_20250701.csv
+  ./run_bq_standard.sh top30 20240701 20250701
 fi
 echo "Restoring BigQuery output"
-cp data/data_top30_without_committers_projects_20250101_20260101.csv data/unlimited.csv
+cp data/data_top30_projects_20240701_20250701.csv data/unlimited.csv
 echo "Adding Linux kernel data"
-ruby add_linux.rb data/unlimited.csv data/data_linux.csv 2025-01-01 2026-01-01
+ruby add_linux.rb data/unlimited.csv data/data_linux.csv 2024-07-01 2025-07-01
 # Don't forget to add exception to map/ranges.csv when adding projects pulled with different BigQuery (specially with 0s for issues, PRs etc)
-echo "Adding/Updating LF Projects"
-ruby merger.rb data/unlimited.csv data/data_lf_projects_20250101_20260101.csv
 echo "Adding/Updating CNCF Projects"
-ruby merger.rb data/unlimited.csv data/data_cncf_projects_20250101_20260101.csv
+ruby merger.rb data/unlimited.csv data/data_cncf_projects_20240701_20250701.csv
 echo "Analysis"
 FORKS_FILE=all_forks.json ruby analysis.rb data/unlimited.csv projects/unlimited_both.csv map/hints.csv map/urls.csv map/defmaps.csv map/skip.csv map/ranges_sane.csv
 echo "Prioritizing LF projects data"
-PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_lf_20250101_20260101.csv -1
+PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_lf_20240701_20250701.csv -1
 echo "Prioritizing CNCF projects data"
-PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_cncf_20250101_20260101.csv -1
+PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_cncf_20240701_20250701.csv -1
 echo "Generating Projects Ranks statistics"
 ./shells/report_cncf_project_ranks.sh
 ./shells/report_other_project_ranks.sh
 ./report_top_projects.sh
-mkdir reports/20250101_20260101/
-cp reports/top_projects_by_*.txt reports/20250101_20260101/
-cp reports/*_ranks.txt reports/20250101_20260101/
+mkdir reports/20240701_20250701/
+cp reports/top_projects_by_*.txt reports/20240701_20250701/
+cp reports/*_ranks.txt reports/20240701_20250701/
 echo "Truncating results to Top 500"
 cat ./projects/unlimited_both.csv | head -n 501 > tmp && mv tmp ./projects/unlimited.csv
 echo "All done"
--- shells/unlimited_both.sh
+++ shells/unlimited_both.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 if [ ! -z "GENERATE" ]
 then
-  # ./run_bq_standard.sh top30 20240701 20250101
-  # ./run_bq_standard.sh top30 20250101 20250701
-  # OUT=data/data_top30_projects_20240701_20250701.csv ./merge_bq.rb data/data_top30_projects_20240701_20250101.csv data/data_top30_projects_20250101_20250701.csv
-  ./run_bq_standard.sh top30 20240701 20250701
+  # ./run_bq_standard.sh top30_without_committers 20250701 20260101
+  # ./run_bq_standard.sh top30_without_committers 20260101 20260701
+  # ./run_bq_standard.sh top30_without_committers 20250701 20260101; ./run_bq_standard.sh top30_without_committers 20260101 20260701; OUT=data/data_top30_without_committers_projects_20250701_20260701.csv ./merge_bq.rb data/data_top30_without_committers_projects_20250701_20260101.csv data/data_top30_without_committers_projects_20260101_20260701.csv
+  ./run_bq_standard.sh top30_without_committers 20250701 20260701
+  ./tools/enrich_authors/enrich_authors -in data/data_top30_without_committers_projects_20250701_20260701.csv -out data/data_top30_without_committers_projects_20250701_20260701.enriched.csv -from 2025-07-01 -to 2026-07-01 -forks all_forks.json -tmp ./tmp -threads 8
+  cp data/data_top30_without_committers_projects_20250701_20260701.csv data/data_top30_without_committers_projects_20250701_20260701.raw.csv
+  cp data/data_top30_without_committers_projects_20250701_20260701.enriched.csv data/data_top30_without_committers_projects_20250701_20260701.csv
 fi
 echo "Restoring BigQuery output"
-cp data/data_top30_projects_20240701_20250701.csv data/unlimited.csv
+cp data/data_top30_without_committers_projects_20250701_20260701.csv data/unlimited.csv
 echo "Adding Linux kernel data"
-ruby add_linux.rb data/unlimited.csv data/data_linux.csv 2024-07-01 2025-07-01
-# Don't forget to add exception to map/ranges.csv when adding projects pulled with different BigQuery (specially with 0s for issues, PRs etc)
+ruby add_linux.rb data/unlimited.csv data/data_linux.csv 2025-07-01 2026-07-01
 echo "Adding/Updating CNCF Projects"
-ruby merger.rb data/unlimited.csv data/data_cncf_projects_20240701_20250701.csv
+ruby merger.rb data/unlimited.csv data/data_cncf_projects_20250701_20260701.csv
 echo "Analysis"
+export RUBYOPT='-EASCII-8BIT:ASCII-8BIT'
+# was map/ranges_unlimited.csv
 FORKS_FILE=all_forks.json ruby analysis.rb data/unlimited.csv projects/unlimited_both.csv map/hints.csv map/urls.csv map/defmaps.csv map/skip.csv map/ranges_sane.csv
 echo "Prioritizing LF projects data"
-PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_lf_20240701_20250701.csv -1
+PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_lf_20250701_20260701.csv -1
 echo "Prioritizing CNCF projects data"
-PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_cncf_20240701_20250701.csv -1
+PROJFMT=1 ruby update_projects.rb projects/unlimited_both.csv ./projects/projects_cncf_20250701_20260701.csv -1
 echo "Generating Projects Ranks statistics"
 ./shells/report_cncf_project_ranks.sh
 ./shells/report_other_project_ranks.sh
 ./report_top_projects.sh
-mkdir reports/20240701_20250701/
-cp reports/top_projects_by_*.txt reports/20240701_20250701/
-cp reports/*_ranks.txt reports/20240701_20250701/
+mkdir reports/20250701_20260701/
+cp reports/top_projects_by_*.txt reports/20250701_20260701/
+cp reports/*_ranks.txt reports/20250701_20260701/
 echo "Truncating results to Top 500"
 cat ./projects/unlimited_both.csv | head -n 501 > tmp && mv tmp ./projects/unlimited.csv
 echo "All done"
